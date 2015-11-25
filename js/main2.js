2acf6bc2c39d8a2bede452b09c4aed8da78ff563
Nik Volkov <freezemandix@ya.ru>
2015-11-25 15:16:45 +0700

    presecond ver

--- js/main2.js
+++ js/main2.js
@@ -1,15 +1,16 @@
 var Storage = window.localStorage;
 
 
-var Task = function(name) {
+var Task = function(name, date, tomatos) {
     // Task name
     this.name = name;
     
     // Complete tomatos
-    this.tomatos = 0;
+    var t = tomatos || 0;
+    this.tomatos = ko.observable( 0 );
     
     // Creation date of task (unique)
-    this.date = Date.now();
+    this.date = date || Date.now();
     
     this.toString = function() {
         return JSON.stringify(this);
@@ -34,42 +35,90 @@ var Task = function(name) {
 
 //Storage.setItem( testTask.date, testTask );
 //
+
+//for ( var i = 0; i < 10000; i++ ) {
+//    var testTask = new Task('Тестовая задача');
+//    testTask.sync();
+//    //console.log(i, testTask.date)
+//}
+
+//Storage.clear();
+console.info( Storage.length )
 /////////////////////////////////////////////
 
 
 
 var Timer = function() {
     // Интервал для работы, сек
-    this.workTime = ko.observable(60 * 25);
+    this.workTime = ko.observable(60 * 2);
     
     // Интервал для отдыха, сек
-    this.relaxTime = ko.observable(60 * 5);
+    this.relaxTime = ko.observable(60 * 1);
     
     // Интервал для длительного отдыха, сек
-    this.bigRelaxTime = ko.observable(60 * 20);
+    this.bigRelaxTime = ko.observable(60 * 5);
+    
+    // Текущая задача
+    this.currentTask = ko.observable({ name: 'ttt', tomatos: 5 });
+    
+    this.currentTime = 0;
     
     // Состояние, в котором находится таймер [stopped, working, relaxing, bigrelaxing]
-    this.state = 'stopped'; // TODO: function
+    this.state = 'stopped';
+    
+    // Определяет тип паузы (интервала для отдыха)
+    this.getPause = function() {
+        var t = this.currentTask();
+        var p = undefined;
+        var s = 'working';
+        
+        if ( (t.tomatos+1) % 4 === 0 ) {
+            p = this.bigRelaxTime();
+            s = 'bigrelaxing';
+        } else {
+            p = this.relaxTime();
+            s = 'relaxing';
+        }
+        
+        return {
+                time: p,
+                state: s,
+            };
+    }.bind(this);
     
     // Список задач на день
     this.tasks = ko.observableArray([]);
     
+    this.taskListBuilding = ko.observable(false);
+    
     // Обновление списка задач на день
     this.tasksSync = function() {
+        this.taskListBuilding(true);
+        phonon.preloader('#taskListBuilding').show();
+        
         var currDate = new Date();
         this.tasks.removeAll();
-        for ( var i = 0; i < Storage.length; i++ ) {
-            var taskDate = new Date( parseInt(Storage.key(i)) );
-            if (
-                    taskDate.getFullYear() === currDate.getFullYear()
-                    &&
-                    taskDate.getMonth() === currDate.getMonth()
-                    &&
-                    taskDate.getDate() === currDate.getDate()
-                ) {
-                this.tasks.push( JSON.parse( Storage.getItem( Storage.key(i) ) ) );
+        try {
+            for ( var i = 0; i < Storage.length; i++ ) {
+                var taskKey = Storage.key(i);   // ключ == date
+                var taskDate = new Date( parseInt( taskKey ) );
+                if (
+                        taskDate.getFullYear() === currDate.getFullYear()
+                        &&
+                        taskDate.getMonth() === currDate.getMonth()
+                        &&
+                        taskDate.getDate() === currDate.getDate()
+                    ) {
+                    var task_data = JSON.parse( Storage.getItem( taskKey ) );
+                    //task.tomatos = ko.observable( task.tomatos );
+                    var task = new Task( task_data.name, task_data.date, task_data.tomatos );
+                    this.tasks.push( task );
+                }
             }
-        }
+        } catch(e) {}
+        
+        this.taskListBuilding(false);
+        phonon.preloader('#taskListBuilding').hide();
     }.bind(this);
     this.tasksSync();
     
@@ -79,16 +128,94 @@ var Timer = function() {
         taskNameField.value = taskNameField.value.trim();
         if ( taskNameField.value.length > 0 ) {
             var task = new Task( taskNameField.value );
-            Storage.setItem( task.date, task );
+            task.sync();
         }
         taskNameField.value = '';
         phonon.panel('#addTaskPanel').close();
         this.tasksSync();
     }.bind(this);
     
+    // Запуск таймера
     this.startTimer = function(task) {
         console.log('timer', task, this)
-        log( task.toString() )
+        //if ( this.status === 'working' || this.state === 'relaxing' || this.state === 'bigrelaxing' ) {
+        //    clearInterval( this.timerInterval );
+        //}
+        
+        window.plugins.insomnia.keepAwake();    // не гасить экран
+        
+        this.currentTask(task);
+        
+        this.state = 'working';
+        this.timer = {
+            start:   Date.now(),
+            current: 0,
+            full:    this.workTime(),
+        }
+        
+        
+        //this.timerInterval = setInterval(function() {
+        //    if (
+        //            window.currentTime === 0
+        //            &&
+        //            (this.state === 'relaxing' || this.state === 'bigrelaxing')
+        //        ) {
+        //        clearInterval( this.timerInterval );
+        //        window.plugins.insomnia.allowSleepAgain();
+        //    } else {
+        //        window.currentTime--;
+        //    }
+        //    
+        //    timer.textContent = Math.floor( window.currentTime / 60 ) + ':' + window.currentTime % 60;
+        //}, 1000);
+        this.timerCont = document.querySelector('#timer .time');
+        this.updateTimer(200);
+    }.bind(this);
+    
+    // Обновление значений таймера
+    this.updateTimer = function( refresh_msecs ) {
+        this.timer.current = Date.now();
+        // Оставшееся время в сек.
+        var t = this.timer.full - ( (this.timer.current - this.timer.start) / 1000 );
+        
+        var mins = Math.floor( t / 60 ); mins = (mins < 0) ? 0 : mins;
+        var secs = Math.floor( t % 60 ); secs = (secs < 0) ? 0 : secs;
+        this.timerCont.textContent = pad(mins.toFixed(0), 2) + ':' + pad(secs.toFixed(0), 2);
+        
+        if ( t <= 0 ) {
+            if ( this.state === 'working' ) {
+                this.timer.start = Date.now();
+                this.timer.full = this.getPause().time;
+                this.state = this.getPause().state;
+                
+                console.log(this)
+                
+                var task = this.currentTask();
+                console.log(task)
+                task.tomatos++;
+                Storage.setItem( task.date, JSON.stringify(task) );
+                console.log(task)
+                
+                setTimeout( this.updateTimer, refresh_msecs );  // запуск таймера для паузы
+            }
+        } else {
+            setTimeout( this.updateTimer, refresh_msecs );  // продолжаем...
+        }
     }.bind(this);
 }
 ko.applyBindings( new Timer() );
+
+
+
+
+
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    
+    if ( n.length < width ) {
+        n = new Array(width - n.length + 1).join(z) + n;
+    }
+    
+    return n;
+}
